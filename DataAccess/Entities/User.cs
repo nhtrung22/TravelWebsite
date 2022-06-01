9624dbc697e332dbb2321118febd5f6a883c987c
Linhut <37008970+nhtrung22@users.noreply.github.com>
2022-06-01 21:38:10 +0700

    pc

--- DataAccess/Entities/User.cs
+++ DataAccess/Entities/User.cs
@@ -20,8 +20,6 @@ namespace TravelWebsite.DataAccess.Entities
 
         public string Email { get; set; }
 
-        //public string Token { get; set; }
-
         public string Address { get; set; }
 
         public string PhoneNumber { get; set; }
