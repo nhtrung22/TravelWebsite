b7a3f68a646004e73d4c5a4c8c299496788c82e8
duc <levanduc094@gmail.com>
2022-06-20 22:47:37 +0700

    update

--- TravelWebsite.Business/Models/DTO/PropertyDTO.cs
+++ TravelWebsite.Business/Models/DTO/PropertyDTO.cs
@@ -5,11 +5,12 @@ namespace TravelWebsite.Business.Models.DTO
     public class PropertyDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string ShortDicription { get; set; } = default!;
-        public decimal Latitude { get; set; }
-        public decimal Longtitude { get; set; }
+        public string Name { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string Discription { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+        public int NumberOfAdults { get; set; }
+        public int NumberOfKids { get; set; }
         public PropertyTypeDTO Type { get; set; } = default!;
         public CityDTO City { get; set; } = default!;
         public IEnumerable<PropertyImageDTO> Images { get; set; } = default!;
